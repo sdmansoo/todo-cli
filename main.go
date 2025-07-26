214ea1aff1d6ea255e1d3ee1004c0a6a3b978efc
Safwan Mansoor <sdmansoo@ucsc.edu>
2025-07-25 20:15:01 -0700

    add create and delete functions

--- main.go
+++ main.go
@@ -4,34 +4,60 @@ import "fmt"
 
 type Task struct {
 	Title       string
+	Description string
 	Completed   bool
 	Priority    int
-	Description string
 }
 
 func main() {
 	task1 := Task{
 		Title:       "Learn GoLang",
+		Description: "get used to the syntax and features of golang",
 		Completed:   false,
 		Priority:    1,
-		Description: "get used to the syntax and features of golang",
 	}
 
 	task2 := Task{
 		Title:       "Push code to repo",
+		Description: "archive work history in github",
 		Completed:   true,
 		Priority:    2,
-		Description: "archive work history in github",
 	}
 
-	tasks := []Task{task1, task2}
+	task3 := addTask("CRUD", "add create,update,read,delete functions", 2)
 
-	for _, v := range tasks {
-		fmt.Print(stringTodo(v))
+	task4 := addTask("Fake", "delete this task", 3)
+
+	tasks := []Task{task1, task2, task3, task4}
+
+	for i, v := range tasks {
+		fmt.Printf("%d ", i+1)
+		fmt.Print(stringTask(v))
+	}
+
+	tasks = deleteTask(tasks, 4)
+	fmt.Println("\ndeleting task 4\n ")
+
+	for i, v := range tasks {
+		fmt.Printf("%d ", i+1)
+		fmt.Print(stringTask(v))
 	}
 }
 
-func stringTodo(t Task) string {
+func stringTask(t Task) string {
 	msg := fmt.Sprintf("\n[ %s ]\n %s \n Priority: %d\n", t.Title, t.Description, t.Priority)
 	return msg
+}
+
+func addTask(title string, description string, priority int) Task {
+	return Task{
+		Title:       title,
+		Description: description,
+		Completed:   false,
+		Priority:    priority,
+	}
+}
+
+func deleteTask(taskList []Task, index int) []Task {
+	return append(taskList[:index-1], taskList[index:]...)
 }
